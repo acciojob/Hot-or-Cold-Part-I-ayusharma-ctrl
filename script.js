33199d634b6050f8b59065840650ecf263cdc6cb
ayusharma-ctrl <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-02-21 11:33:06 +0530

    completed

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
+var input = document.getElementById('guess')
+var respond = document.getElementById('respond')
+var btn = document.getElementById('btn')
+var num = document.getElementById('num')
 
+function showRandom(){
+	const randomNum = getRandom(-20, 20);
+	num.textContent = randomNum
+	if(input.value===randomNum){
+		respond.textContent = "Hot"
+	}
+	else{
+		respond.textContent = "Cold"
+	}
+	input.value = ""
+}
+
+btn.addEventListener('click',showRandom)
+
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
